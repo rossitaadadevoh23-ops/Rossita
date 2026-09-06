d6a6e256216404336a46955e3353ecd71c782606
rossitaadadevoh23-ops <rossitaadadevoh23@gmail.com>
2026-09-06 12:22:14 -0900

    Question 1

---
+++ .vscode/question1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Temperature in Fahrenheit = %.2f\n", fahrenheit);
+
+	return 0;
+}
